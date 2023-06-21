3608c6e12957f91fc929b07127acf42b8ecba920
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-21 12:11:35 -0400

    added a second array to use for a test

---
+++ script.js
@@ -0,0 +1,8 @@
+//Arrays Problem 4
+/*
+Write a JavaScript function to remove duplicate numbers from an array of numbers.
+
+Example array: [1,2,2,4,5,4,7,8,7,3,6]
+
+NOTE: Do NOT use the Set method.
+*/

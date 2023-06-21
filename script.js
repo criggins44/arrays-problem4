4ab98e6265ce7f55544400a97ddd6a5b9fbd3b2e
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-21 13:05:18 -0400

    experimented with for each method and includes method to write the function to remove duplicates

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ Example array: [1,2,2,4,5,4,7,8,7,3,6]
 
 NOTE: Do NOT use the Set method.
 */
+
+const myArray = [1,2,2,4,5,4,7,8,7,3,6];
+const secondArray = ['red', 'red', 'white', 'blue', 'blue', 'white', 'green'];
+
+let arr = ["scale", "happy", "strength",
+            "peace", "happy", "happy"];
+ 
+    function removeDuplicates(arr) {
+        let unique = [];
+        arr.forEach(element => {
+            if (!unique.includes(element)) {
+                unique.push(element);
+            }
+        });
+        return unique;
+    }
+ 
+    //console.log(removeDuplicates(secondArray));
+
+    myFunction = array => {
+    	let noDuplicateArray = [];
+    	array.forEach(element => {
+    		if(!noDuplicateArray.includes(element)) {
+    			noDuplicateArray.push(element);
+    		}
+    	});
+    	return noDuplicateArray;
+    }
+
+    //console.log(myFunction(secondArray));
+    console.log(secondArray.forEach(element + ' ball');

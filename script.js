e9a8e7a4c54b3454bb543e32e1f45aa89de69c64
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-21 14:42:33 -0400

    added comments to explain logic

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ const secondArray = ['red', 'red', 'white', 'blue', 'blue', 'white', 'green'];
 
 myFunction = array => {
 	let noDuplicatesArray = [];
+//an empty array that will store elements based on the conditions below
 
 	array.forEach(element => {
 		if(!noDuplicatesArray.includes(element)){noDuplicatesArray.push(element)}
 	});
+/*if each element being stored in the noDuplicatesArray is not already included,
+	it will be added to the array using the push method
+*/
 		return noDuplicatesArray;
+/*returns the words that don't already exist in the array that is being filled with 
+		elements from the array passed into the function
+		*/
 }
+
 console.log(myFunction(myArray));
